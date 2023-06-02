039997041a0c24cb4847f869070253a5046d87f2
James Viner Esq <jrviner2000@gmail.com>
2023-06-02 15:32:36 -0400

    Add function to create HTTP response headers

--- ashti.c
+++ ashti.c
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <sysexits.h>
+#include <time.h>
 #include <unistd.h>
 #include <stdlib.h>
 
@@ -26,217 +27,238 @@ char *uid_to_str(uid_t uid);
 int validate_root_dir(const char *directory);
 char *extract_filename(const char *request, int *err);
 bool validate_request_method(const char *request);
-bool validate_request_legality(const char *root, const char *target, char **full_name);
+bool validate_request_legality(const char *root, const char *target,
+			       char **full_name);
+char *prepare_headers(const size_t server_code,
+		      const char *filename, const off_t filesize);
 
 int main(int argc, char *argv[])
 {
-    if(argc == 1 || argc > 2) {
-        fprintf(stderr, "Usage: %s server_root\n", argv[0]);
-        return EX_USAGE;
-    }
-    int rc = validate_root_dir(argv[1]);
-    if (rc == -1) {
-        return EX_USAGE;
-    } else if (rc == EX_OSERR) {
-        perror("Failed to allocate memory for buffer");
-        return EX_OSERR;
-    }
+	if (argc == 1 || argc > 2) {
+		fprintf(stderr, "Usage: %s server_root\n", argv[0]);
+		return EX_USAGE;
+	}
+	int rc = validate_root_dir(argv[1]);
+	if (rc == -1) {
+		return EX_USAGE;
+	} else if (rc == EX_OSERR) {
+		perror("Failed to allocate memory for buffer");
+		return EX_OSERR;
+	}
 
-    struct addrinfo hints = {0};
-    hints.ai_family = PF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    // Hints that we want to bind to a socket in server scenario
-    hints.ai_flags = AI_PASSIVE;
+	struct addrinfo hints = { 0 };
+	hints.ai_family = PF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	// Hints that we want to bind to a socket in server scenario
+	hints.ai_flags = AI_PASSIVE;
 
-    uid_t uid = getuid();
-    char *uid_str = uid_to_str(uid);
-    if (!uid_str) {
-        perror("Failed to allocate memory for buffer");
-        return EX_OSERR;
-    }
-    struct addrinfo *results;
-    int err = getaddrinfo(NULL, uid_str, &hints, &results);
-    free(uid_str);
-    if (err != 0) {
-        fprintf(stderr, "Cannot get address: %s\n", gai_strerror(err));
-        return EX_NOHOST;
-    }
+	uid_t uid = getuid();
+	char *uid_str = uid_to_str(uid);
+	if (!uid_str) {
+		perror("Failed to allocate memory for buffer");
+		return EX_OSERR;
+	}
+	struct addrinfo *results;
+	int err = getaddrinfo(NULL, uid_str, &hints, &results);
+	free(uid_str);
+	if (err != 0) {
+		fprintf(stderr, "Cannot get address: %s\n", gai_strerror(err));
+		return EX_NOHOST;
+	}
 
-    int sd = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
-    if(sd < 0) {
-        perror("Could not create socket");
-        freeaddrinfo(results);
-        return EX_OSERR;
-    }
-    int enable = 1;
-    if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0) {
-        perror("Could not mark socket for address reuse");
-    }
+	int sd = socket(results->ai_family, results->ai_socktype,
+			results->ai_protocol);
+	if (sd < 0) {
+		perror("Could not create socket");
+		freeaddrinfo(results);
+		return EX_OSERR;
+	}
+	int enable = 1;
+	if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0) {
+		perror("Could not mark socket for address reuse");
+	}
 
-    err = bind(sd, results->ai_addr, results->ai_addrlen);
-    if(err < 0) {
-        perror("Could not bind socket");
-        close(sd);
-        freeaddrinfo(results);
-        return EX_OSERR;
-    }
-    freeaddrinfo(results);
+	err = bind(sd, results->ai_addr, results->ai_addrlen);
+	if (err < 0) {
+		perror("Could not bind socket");
+		close(sd);
+		freeaddrinfo(results);
+		return EX_OSERR;
+	}
+	freeaddrinfo(results);
 
-    // Backlog of 5 is typical
-    err = listen(sd, 5);
-    if(err < 0) {
-        perror("Could not listen on socket");
-        close(sd);
-        return EX_OSERR;
-    }
+	// Backlog of 5 is typical
+	err = listen(sd, 5);
+	if (err < 0) {
+		perror("Could not listen on socket");
+		close(sd);
+		return EX_OSERR;
+	}
 
-    for(;;) {
-        struct sockaddr_storage client;
-        socklen_t client_sz = sizeof(client);
-        char addr[INET6_ADDRSTRLEN];
+	for (;;) {
+		struct sockaddr_storage client;
+		socklen_t client_sz = sizeof(client);
+		char addr[INET6_ADDRSTRLEN];
 
-        int remote = accept(sd, (struct sockaddr *)&client, &client_sz);
+		int remote = accept(sd, (struct sockaddr *)&client, &client_sz);
 
-        unsigned short port = 0;
-        if(client.ss_family == AF_INET6) {
-            inet_ntop(client.ss_family,
-                    &((struct sockaddr_in6 *)&client)->sin6_addr, addr, sizeof(addr));
-            port = ntohs(((struct sockaddr_in6 *)&client)->sin6_port);
-        } else {
-            inet_ntop(client.ss_family, &((struct sockaddr_in *)&client)->sin_addr,
-                    addr, sizeof(addr));
-            port = ntohs(((struct sockaddr_in *)&client)->sin_port);
-        }
-        printf("Received from %s:%hu\n", addr, port);
+		unsigned short port = 0;
+		if (client.ss_family == AF_INET6) {
+			inet_ntop(client.ss_family,
+				  &((struct sockaddr_in6 *)&client)->sin6_addr,
+				  addr, sizeof(addr));
+			port =
+			    ntohs(((struct sockaddr_in6 *)&client)->sin6_port);
+		} else {
+			inet_ntop(client.ss_family,
+				  &((struct sockaddr_in *)&client)->sin_addr,
+				  addr, sizeof(addr));
+			port = ntohs(((struct sockaddr_in *)&client)->sin_port);
+		}
+		printf("Received from %s:%hu\n", addr, port);
 
-        pid_t child_pid = fork();
-        if(child_pid < 0) {
-            perror("Could not fork process");
-            close(remote);
-            continue;
-        } else if(child_pid == 0) {
-            // Child process
-            close(sd);  // Close the listening socket in the child process
+		pid_t child_pid = fork();
+		if (child_pid < 0) {
+			perror("Could not fork process");
+			close(remote);
+			continue;
+		} else if (child_pid == 0) {
+			// Child process
+			close(sd);	// Close the listening socket in the child process
 
-            // seems like a reasonable buffer size
-            size_t buffer_size = 1024;
-            char *buffer = malloc(buffer_size);
-            if(buffer == NULL) {
-                perror("Failed to allocate memory for buffer");
-                close(remote);
-                return EX_OSERR;
-            }
+			// seems like a reasonable buffer size
+			size_t buffer_size = 1024;
+			char *buffer = malloc(buffer_size);
+			if (buffer == NULL) {
+				perror("Failed to allocate memory for buffer");
+				close(remote);
+				return EX_OSERR;
+			}
 
-            size_t total_received = 0;
-            ssize_t received = recv(remote, buffer + total_received, buffer_size - total_received - 1, 0);
-            if (received > 0) {
-                total_received += (size_t)received;
-                if(total_received >= buffer_size - 1) {
-                    // Expand the buffer size using realloc
-                    buffer_size *= 2;
-                    char *new_buffer = realloc(buffer, buffer_size);
-                    if(new_buffer == NULL) {
-                        perror("Failed to reallocate memory for buffer");
-                        free(buffer);
-                        close(remote);
-                        return EX_OSERR;
-                    }
-                    buffer = new_buffer;
-                    }
-            } else if (received < 0) {
-                perror("Unable to receive");
-                // Error?
-            }
+			size_t total_received = 0;
+			ssize_t received = recv(remote, buffer + total_received,
+						buffer_size - total_received -
+						1, 0);
+			if (received > 0) {
+				total_received += (size_t)received;
+				if (total_received >= buffer_size - 1) {
+					// Expand the buffer size using realloc
+					buffer_size *= 2;
+					char *new_buffer =
+					    realloc(buffer, buffer_size);
+					if (new_buffer == NULL) {
+						perror
+						    ("Failed to reallocate memory for buffer");
+						free(buffer);
+						close(remote);
+						return EX_OSERR;
+					}
+					buffer = new_buffer;
+				}
+			} else if (received < 0) {
+				perror("Unable to receive");
+				// Error?
+			}
+			// Potentially sometimes overwrites a single char?
+			buffer[total_received] = '\0';	// Null-terminate the received message
+			printf("Received message:\n%s\n", buffer);
+			int err = 0;
+			char *filename = extract_filename(buffer, &err);
+			if (!filename) {
+				printf("error2");
+				// error handling for 400 error and malloc issues
+			}
+			char *full_name = NULL;
+			bool valid =
+			    validate_request_legality(argv[1], filename,
+						      &full_name);
+			if (!valid) {
+				printf("Error1");
+				// error handling for generic errors and malloc issues
+			}
+			// int fd = open(filename, O_RDONLY);
+			// struct stat stat_buffer;
+			// stat(filename, &stat_buffer);
+			// puts("HERE?");
+			// sendfile(remote, fd, NULL, stat_buffer.st_size);
 
-            // Potentially sometimes overwrites a single char?
-            buffer[total_received] = '\0';  // Null-terminate the received message
-            printf("Received message:\n%s\n", buffer);
-            int err = 0;
-            char *filename = extract_filename(buffer, &err);
-            if (!filename) {
-                printf("error2");
-                // error handling for 400 error and malloc issues
-            }
-            char *full_name = NULL;
-            bool valid = validate_request_legality(argv[1], filename, &full_name);
-            if (!valid) {
-                printf("Error1");
-                // error handling for generic errors and malloc issues
-            }
-            // int fd = open(filename, O_RDONLY);
-            // struct stat stat_buffer;
-            // stat(filename, &stat_buffer);
-            // puts("HERE?");
-            // sendfile(remote, fd, NULL, stat_buffer.st_size);
+			// free(filename);
+			// free(buffer);  // Free the dynamically allocated buffer
+			// After the validate_request_legality call
+			printf("-> %s\n", full_name);
+			// Get a file descriptor for sendfile() call later
+			int fd = open(full_name, O_RDONLY);
+			if (fd < 0) {
+				perror("Failed to open file");
+				free(full_name);
+				free(filename);
+				free(buffer);
+				close(remote);
+				return -1;
+			}
+			// Retrieve filesize for sendfile() call
+			struct stat stat_buffer;
+			if (stat(full_name, &stat_buffer) != 0) {
+				perror("Failed to retrieve file information");
+				free(full_name);
+				free(filename);
+				free(buffer);
+				close(fd);
+				close(remote);
+				return -1;
+			}
+			free(full_name);
 
-            // free(filename);
-            // free(buffer);  // Free the dynamically allocated buffer
-            // After the validate_request_legality call
-            printf("-> %s\n", full_name);
-            int fd = open(full_name, O_RDONLY);
-            if (fd < 0) {
-                perror("Failed to open file");
-                free(full_name);
-                free(filename);
-                free(buffer);
-                close(remote);
-                return -1;
-            }
+			char *header =
+			    prepare_headers(200, filename, stat_buffer.st_size);
+			send(remote, header, strlen(header), 0);
+			free(header);
+			// Send file to remote client
+			ssize_t bytes_sent =
+			    sendfile(remote, fd, NULL, stat_buffer.st_size);
+			if (bytes_sent == -1) {
+				perror("Failed to send file");
+				free(filename);
+				free(buffer);
+				close(fd);
+				close(remote);
+				return -1;
+			}
+			// devprint
+			// printf("Sent %zd bytes\n", bytes_sent);
 
-            struct stat stat_buffer;
-            if (stat(full_name, &stat_buffer) != 0) {
-                perror("Failed to retrieve file information");
-                free(full_name);
-                free(filename);
-                free(buffer);
-                close(fd);
-                close(remote);
-                return -1;
-            }
-            free(full_name);
-            ssize_t bytes_sent = sendfile(remote, fd, NULL, stat_buffer.st_size);
-            if (bytes_sent == -1) {
-                perror("Failed to send file");
-                free(filename);
-                free(buffer);
-                close(fd);
-                close(remote);
-                return -1;
-            }
+			// Cleanup
+			free(filename);
+			free(buffer);
+			close(fd);
+			close(remote);
+			putchar('\n');
 
-            printf("Sent %zd bytes\n", bytes_sent);
+			return EXIT_SUCCESS;	// Exit the child process
+		} else {
+			// Parent process
+			close(remote);	// Close the remote socket in the parent process
+		}
+	}
 
-            free(filename);
-            free(buffer);
-            close(fd);
-            close(remote);
-            putchar('\n');
-
-            return 0;  // Exit the child process
-        } else {
-            // Parent process
-            close(remote);  // Close the remote socket in the parent process
-        }
-    }
-
-    close(sd);
+	close(sd);
 }
 
 char *uid_to_str(uid_t uid)
 {
-    // Max port: 65535 + '\0'
-    char *uid_str = malloc(sizeof(*uid_str) * 7);
-    if (!uid_str) {
-        return NULL;
-    }
-    snprintf(uid_str, 7, "%d", uid);
-    return uid_str;
+	// Max port: 65535 + '\0'
+	char *uid_str = malloc(sizeof(*uid_str) * 7);
+	if (!uid_str) {
+		return NULL;
+	}
+	snprintf(uid_str, 7, "%d", uid);
+	return uid_str;
 }
 
 int validate_root_dir(const char *directory)
 {
-    // Return code; success by default
-    int rc = 0;
+	// Return code; success by default
+	int rc = 0;
 	char *dir_cpy = strdup(directory);
 	if (!dir_cpy) {
 		return EX_OSERR;
@@ -245,14 +267,14 @@ int validate_root_dir(const char *directory)
 	DIR *dir = opendir(dir_cpy);
 	if (dir) {
 		closedir(dir);
-		
+
 		size_t dir_len = strlen(directory);
 		// strlen(directory) + '\0' + "/cgi-bin" at longest
 		size_t new_dir_len = dir_len + 9;
 		char *tmp = realloc(dir_cpy, new_dir_len);
 		if (!tmp) {
 			rc = EX_OSERR;
-            goto cleanup;
+			goto cleanup;
 		}
 		dir_cpy = tmp;
 
@@ -260,15 +282,14 @@ int validate_root_dir(const char *directory)
 			// Prune trailing slash if present
 			dir_cpy[dir_len - 1] = '\0';
 		}
-
 		// Sub-directories to check for
 		const char *dirs[2] = { "/cgi-bin", "/www" };
 		// Check for each sub-directory
 		for (size_t i = 0; i < 2; ++i) {
 			char *tmp = malloc(sizeof(*tmp) * new_dir_len);
 			if (!tmp) {
-                rc = EX_OSERR;
-                goto cleanup;
+				rc = EX_OSERR;
+				goto cleanup;
 			}
 			snprintf(tmp, new_dir_len, "%s%s", dir_cpy, dirs[i]);
 			DIR *sub_dir = opendir(tmp);
@@ -277,7 +298,7 @@ int validate_root_dir(const char *directory)
 				// Format perror
 				perror(" \b");
 				free(tmp);
-                rc = -1;
+				rc = -1;
 				goto cleanup;
 			}
 			closedir(sub_dir);
@@ -288,10 +309,10 @@ int validate_root_dir(const char *directory)
 		fprintf(stderr, "Unable to find %s", dir_cpy);
 		// Format perror
 		perror(" \b");
-        rc = -1;
+		rc = -1;
 	}
 
-cleanup:
+ cleanup:
 	free(dir_cpy);
 	return rc;
 }
@@ -300,14 +321,14 @@ char *extract_filename(const char *request, int *err)
 {
 	char *req_cpy = strdup(request);
 	if (!req_cpy) {
-        *err = EX_OSERR;
+		*err = EX_OSERR;
 		return NULL;
 	}
 
 	if (!validate_request_method(req_cpy)) {
 		// Request did not begin with acceptable method
 		// TODO: return 400 error to client
-        *err = -1;
+		*err = -1;
 		free(req_cpy);
 		return NULL;
 	}
@@ -319,7 +340,7 @@ char *extract_filename(const char *request, int *err)
 	sub_str = strtok_r(NULL, " \n", &saveptr);
 	char *filename = strdup(sub_str);
 	if (!filename) {
-        *err = EX_OSERR;
+		*err = EX_OSERR;
 		free(req_cpy);
 		return NULL;
 	}
@@ -347,7 +368,8 @@ bool validate_request_method(const char *request)
 	return true;
 }
 
-bool validate_request_legality(const char *root, const char *target, char **full_name)
+bool validate_request_legality(const char *root, const char *target,
+			       char **full_name)
 {
 	char *root_path = NULL;
 	char *full_path = NULL;
@@ -365,7 +387,6 @@ bool validate_request_legality(const char *root, const char *target, char **full
 		// Prune trailing slash if present
 		root_path[root_path_len - 1] = '\0';
 	}
-
 	// Two paths combined + '/www/' + '\0'
 	size_t full_path_len = root_path_len + strlen(target) + 6;
 	full_path = malloc(sizeof(*full_path) * full_path_len);
@@ -376,12 +397,13 @@ bool validate_request_legality(const char *root, const char *target, char **full
 
 	if (!strstr(target, "cgi-bin/")) {
 		// Default directory is "www"
-		snprintf(full_path, full_path_len, "%s/www%s", root_path, target);
+		snprintf(full_path, full_path_len, "%s/www%s", root_path,
+			 target);
 	} else {
 		// If user specifies "cgi-bin", do not add "www"
 		snprintf(full_path, full_path_len, "%s/%s", root_path, target);
 	}
-    *full_name = strdup(full_path);
+	*full_name = strdup(full_path);
 
 	final_path = realpath(full_path, NULL);
 	if (!final_path) {
@@ -400,7 +422,7 @@ bool validate_request_legality(const char *root, const char *target, char **full
 			goto cleanup;
 		}
 		snprintf(tmp, sub_folder_path_len,
-				 "%s%s", root_path, legal_folders[i]);
+			 "%s%s", root_path, legal_folders[i]);
 		if (strstr(final_path, tmp)) {
 			free(tmp);
 			is_legal = true;
@@ -408,11 +430,85 @@ bool validate_request_legality(const char *root, const char *target, char **full
 		}
 		free(tmp);
 	}
-	
 
-cleanup:
+ cleanup:
 	free(root_path);
 	free(full_path);
 	free(final_path);
 	return is_legal;
+}
+
+char *prepare_headers(const size_t server_code,
+		      const char *filename, const off_t filesize)
+{
+	char *header_msg = "";
+	switch (server_code) {
+	case 200:
+		header_msg = "200 OK";
+		break;
+	case 400:
+		header_msg = "400 Bad Request";
+		break;
+	case 403:
+		header_msg = "403 Forbidden";
+		break;
+	case 404:
+		header_msg = "404 Not Found";
+		break;
+	case 405:
+		header_msg = "405 Method Not Allowed";
+		break;
+	case 500:
+		header_msg = "500 Internal Server Error";
+		break;
+	default:
+		header_msg = "418 I'm a teapot";
+	}
+
+	// This will very likely fail if multiple extensions are provided
+	// ex. file.txt.html.gif
+	char *filetype = "";
+	if (server_code == 200) {
+		// Assign filetype based on extension
+		if (strstr(filename, ".txt") || strstr(filename, ".text")) {
+			filetype = "text/plain";
+		} else if (strstr(filename, ".jpg") || strstr(filename, ".jpeg")
+			   || strstr(filename, ".jfif")
+			   || strstr(filename, ".pjpeg")
+			   || strstr(filename, ".pjp")) {
+			filetype = "image/jpeg";
+		} else if (strstr(filename, ".png")) {
+			filetype = "image/png";
+		} else if (strstr(filename, ".gif")) {
+			filetype = "image/gif";
+		} else {
+			// Default to assume html
+			filetype = "text/html";
+		}
+	} else {
+		// All non-OK responses will be in HTML format
+		filetype = "text/html";
+	}
+
+	// Prepare date
+	time_t current_time;
+	struct tm *date;
+	// Date format will always be 29 bytes in length + '\0'
+	char date_buf[30];
+	time(&current_time);
+	date = gmtime(&current_time);
+
+	strftime(date_buf, 30, "%a, %d %b %Y %X GMT", date);
+
+	// Prepare final string
+	char *format_str =
+	    "HTTP/1.1 %s\r\nDate: %s\r\nContent-Type: %s\r\nContent-Length: %d\r\n\r\n";
+
+	// Response header at absolute longest should not exceed 150 bytes
+	char *final_buf = malloc(sizeof(*final_buf) * 150);
+
+	snprintf(final_buf, 150, format_str, header_msg, date_buf, filetype,
+		 filesize);
+
+	return final_buf;
 }
